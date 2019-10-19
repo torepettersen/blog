00760296b992476bdc333c643a970be183035b67
Tore Skog Pettesen <toreskog@live.com>
2019-10-19 13:34:17 +0200

    Open sidebar

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
-	.theme-container.no-sidebar
+	.theme-container(:class='pageClasses', @touchstart='onTouchStart', @touchend="onTouchEnd")
 		
-		Navbar
+		Navbar(v-if='shouldShowNavbar', @toggle-sidebar='toggleSidebar')
 
+		Sidebar(:items='sidebarItems', @toggle-sidebar='toggleSidebar')
 
 		main.page
 			slot(name='header')
@@ -29,12 +30,72 @@
 
 <script>
 import Navbar from '@theme/components/Navbar.vue';
+import Sidebar from '@theme/components/Sidebar.vue';
 import spacetime from 'spacetime';
+import { resolveSidebarItems } from '../util';
 
 export default {
 	components: {
 		Navbar,
+		Sidebar,
 	},
+
+	data () {
+    return {
+      isSidebarOpen: false
+    }
+  },
+  computed: {
+    shouldShowNavbar () {
+      const { themeConfig } = this.$site
+      const { frontmatter } = this.$page
+      if (
+        frontmatter.navbar === false
+        || themeConfig.navbar === false) {
+        return false
+      }
+      return (
+        this.$title
+        || themeConfig.logo
+        || themeConfig.repo
+        || themeConfig.nav
+        || this.$themeLocaleConfig.nav
+      )
+    },
+    shouldShowSidebar () {
+      const { frontmatter } = this.$page
+      return (
+        !frontmatter.home
+        && frontmatter.sidebar !== false
+        && this.sidebarItems.length
+      )
+    },
+    sidebarItems () {
+      return resolveSidebarItems(
+        this.$page,
+        this.$page.regularPath,
+        this.$site,
+        this.$localePath
+      )
+    },
+    pageClasses () {
+      const userPageClass = this.$page.frontmatter.pageClass
+      return [
+        {
+          'no-navbar': !this.shouldShowNavbar,
+          'sidebar-open': this.isSidebarOpen,
+          'no-sidebar': !this.shouldShowSidebar
+        },
+        userPageClass
+      ]
+    }
+  },
+
+	mounted () {
+    this.$router.afterEach(() => {
+      this.isSidebarOpen = false
+    })
+  },
 
 	methods: {
     formatDate(date) {
@@ -48,6 +109,28 @@ export default {
         .replace(/[^a-zA-Z\s]/g, '')
         .replace(/\s+/g, '-')
         .toLowerCase();
+		},
+		toggleSidebar (to) {
+      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
+      this.$emit('toggle-sidebar', this.isSidebarOpen)
+    },
+    // side swipe
+    onTouchStart (e) {
+      this.touchStart = {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY
+      }
+    },
+    onTouchEnd (e) {
+      const dx = e.changedTouches[0].clientX - this.touchStart.x
+      const dy = e.changedTouches[0].clientY - this.touchStart.y
+      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+        if (dx > 0 && this.touchStart.x <= 80) {
+          this.toggleSidebar(true)
+        } else {
+          this.toggleSidebar(false)
+        }
+      }
     }
   }
 }
