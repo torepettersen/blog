8daeca14bb96e45d8f895158a65a9948f353aaaf
Tore Skog Pettesen <toreskog@live.com>
2019-10-27 08:41:06 +0100

    Sign up form

---
+++ .vuepress/theme/components/EmailForm.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+div
+  form.subscribe-form(
+    action='https://dev.us20.list-manage.com/subscribe/post?u=b5cba5b1a8baa382a74542a78&amp;id=ddc2acb3df',
+    method='post'
+  )
+    input(name='email', type='email', placeholder='Your email address')
+    input(type='hidden' name='b_b5cba5b1a8baa382a74542a78_ddc2acb3df' tabindex='-1' value='')
+    button.submit(type='submit', name='subscribe') Subscribe
+</template>
+
+
+
+<style lang="scss" scoped>
+@import "../styles/palette.scss";
+$borderRadius: 0.5rem;
+
+.subscribe-form {
+  display: flex;
+
+  & * {
+    font-size: 0.9rem;
+    line-height: 2rem;
+    padding: 0 1rem;
+    display: inline-block;
+  }
+
+  :first-child {
+    border-radius: $borderRadius 0 0 $borderRadius;
+  }
+
+  :last-child {
+    border-radius: 0 $borderRadius $borderRadius 0;
+  }
+
+  :not(:last-child) {
+    border-right: 0;
+  }
+
+  input {
+    cursor: text;
+    color: #4e6e8e;
+    border: 1px solid $accentColor;
+    outline: none;
+    transition: all 0.2s ease;
+  }
+
+  .submit {
+    cursor: pointer;
+    background-color: $accentColor;
+    border: 0;
+    color: #fff;
+    transition: background-color .1s ease;
+  }
+}
+
+
+</style>
--- .vuepress/theme/layouts/IndexPost.vue
+++ .vuepress/theme/layouts/IndexPost.vue
@@ -1,31 +1,34 @@
 <template lang="pug">
-	Layout
-		template(v-slot:header)
-			.header-image(style="background-image: url(/img/list-cover.jpg)")
-		template
-			.list
-				article.list-item(v-for="page in $pagination.pages")
-					router-link(:to="page.path")
-						h2 {{ page.title }}
-					p {{ page.frontmatter.description || page.description }}
-					template(v-if="page.frontmatter.date")
-						i.fas.fa-clock
-						span  {{ formatDate(page.frontmatter.date) }}&ensp;
-					template(v-if="page.frontmatter.categories && page.frontmatter.categories.length")
-						i.fas.fa-layer-group
-						span  {{ formatCategories(page.frontmatter.categories) }}
+  Layout
+    template(v-slot:header)
+      .header-image(style="background-image: url(/img/list-cover.jpg)")
+    template
+      .list
+        EmailForm
+        article.list-item(v-for="page in $pagination.pages")
+          router-link(:to="page.path")
+            h2 {{ page.title }}
+          p {{ page.frontmatter.description || page.description }}
+          template(v-if="page.frontmatter.date")
+            i.fas.fa-clock
+            span  {{ formatDate(page.frontmatter.date) }}&ensp;
+          template(v-if="page.frontmatter.categories && page.frontmatter.categories.length")
+            i.fas.fa-layer-group
+            span  {{ formatCategories(page.frontmatter.categories) }}
 </template>
 
 <script>
 import Layout from '@theme/layouts/Layout';
+import EmailForm from '@theme/components/EmailForm';
 import spacetime from 'spacetime';
 
 export default {
-	components: {
-		Layout,
-	},
+  components: {
+    Layout,
+    EmailForm,
+  },
 
-	methods: {
+  methods: {
     formatDate(date) {
       return spacetime(date).format('{month-short} {date-ordinal}, {year}');
     },
